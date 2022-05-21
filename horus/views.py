72db88fd0540a8a49dfacf3830a10bc2baa8f86f
woojiahao <woojiahao1234@gmail.com>
2022-05-21 13:14:11 +0800

    Add search results for location page skeleton

--- horus/views.py
+++ horus/views.py
@@ -11,13 +11,20 @@ def search(request):
     """
     Returns the search results of the initial location search
     """
-    # TODO: Add logging for the location searched
-    print(request)
-    return render(request, 'index.html', {'d': 'hi'})
+
+    if request.method == 'POST':
+        # TODO: Add logging for the location searched
+
+        results = ['Hello', 'Pineapple']
+
+        return render(request, 'search.html', {'search': request.POST['location'], 'results': results})
+    else:
+        return render(request, 'index.html', {})
 
 
 def weather(request):
     """
     Returns the weather of the searched location
     """
-    return render(request, )
+    # return render(request, )
+    pass
