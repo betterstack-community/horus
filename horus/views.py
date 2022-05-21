96fb4554ef6ac9f8bf1db7af7b9d9f7b4a7a652d
woojiahao <woojiahao1234@gmail.com>
2022-05-21 10:51:02 +0800

    Add basic view from file

--- horus/views.py
+++ horus/views.py
@@ -1,8 +1,7 @@
 from django.http import HttpResponse
-
-
+from django.shortcuts import render
 from django.template import loader
 
 
 def index(request):
-    return HttpResponse(loader.get_template('index.html').render({}, request))
+    return render(request, 'index.html', {})
