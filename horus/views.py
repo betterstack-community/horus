ca19ba23baf3bdab95afaa1c82c698a007a29808
woojiahao <woojiahao1234@gmail.com>
2022-05-21 13:25:22 +0800

    Add env reading

--- horus/views.py
+++ horus/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
+import environ
+
+env = environ.Env()
+env.read_env(env.str('ENV_PATH', '.env'))
 
 
 def index(request):
@@ -16,6 +20,8 @@ def search(request):
         # TODO: Add logging for the location searched
 
         results = ['Hello', 'Pineapple']
+
+        # TODO: Demonstrate negative example of logging an API key by logging your OpenWeather API key
 
         return render(request, 'search.html', {'search': request.POST['location'], 'results': results})
     else:
