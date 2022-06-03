0b4ddb962b141c7f7230d03ca247f8cca38e8d3a
woojiahao <woojiahao1234@gmail.com>
2022-06-03 22:44:55 +0800

    Use openweather module for retrieving weather

--- horus/views/weather.py
+++ horus/views/weather.py
@@ -1,9 +1,6 @@
-import uuid
-
-import environ
-import pycountry
-import requests
 from django.shortcuts import redirect, render
+from openweather import get_current_weather
+
 
 def weather(request):
     """
@@ -21,26 +18,12 @@ def weather(request):
     # TODO: Log the latitude and longitude
     input = request.POST['location'].split(', ')
 
-    api = 'https://api.openweathermap.org/data/2.5/weather'
-    response = requests.get(
-        api,
-        params={
-            'lat': float(input[1]),
-            'lon': float(input[2]),
-            'appid': openweather_api_key,
-            'units': 'metric'
-        })
-
+    (success, weather) = get_current_weather(
+        input[0], float(input[1]), float(input[2]))
     # TODO: Log response from API
 
-    if not response.ok:
+    if not success:
         # TODO: Log when request failed
-        return render(request, 'weather.html', {'success': False, 'weather': f'Invalid location selected...'})
+        return render(request, 'weather.html', {'success': success})
 
-    weather = {
-        'current': response.json()['weather'][0]['description'].lower(),
-        'temp': response.json()['main']['temp'],
-        'feels_like': response.json()['main']['feels_like'],
-        'location': input[0]
-    }
-    return render(request, 'weather.html', {'success': True, 'weather': weather})
+    return render(request, 'weather.html', {'success': success, 'weather': weather})
