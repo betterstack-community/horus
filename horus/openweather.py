29d705573164fe461521d4064d92123caacfa01d
woojiahao <woojiahao1234@gmail.com>
2022-06-04 20:21:38 +0800

    Update instructions for openweather

--- horus/openweather.py
+++ horus/openweather.py
@@ -26,7 +26,7 @@ key = env('OPEN_WEATHER_API_KEY')
 
 # TODO: Create a StreamHandler to output all logs to the console
 
-# TODO: Create a FileHandler to save all WARNING and above logs to a file
+# TODO: Create a FileHandler to save all WARNING and above logs to a file (hint: set the log level of the FileHandler)
 
 # TODO: Create a Formatter that will output the following format: '<name> at <timestamp> (level) :: <message>'
 
