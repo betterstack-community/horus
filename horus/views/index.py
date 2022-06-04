11f9c6b1db15002239855ba7a8c2571248e207d4
woojiahao <woojiahao1234@gmail.com>
2022-06-04 20:05:49 +0800

    Elaborate on setting up logging in index

--- horus/views/index.py
+++ horus/views/index.py
@@ -13,31 +13,31 @@ We will do so by creating a Logger and attaching a StreamHandler and Formatter, 
 session is created.
 """
 
-# Create a new logger with name 'horus.views.index'
+# Create a new Logger with name 'horus.views.index', this Logger will be used for the entire view
 logger = logging.getLogger('horus.views.index')
 
-# Create a StreamHandler to output all logs to console
-ch = logging.StreamHandler()
+# Set Logger level to include all INFO messages
+logger.setLevel(logging.INFO)
 
-# Set StreamHandler level to include all INFO messages
-ch.setLevel(logging.INFO)
+# Create a StreamHandler to output all logs to the console
+sh = logging.StreamHandler()
 
 # Create a Formatter that will output the following format: '<name> at <timestamp> (<level>) :: <message>'
 formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    '%(name)s  at %(asctime)s (%(levelname)s) :: %(message)s')
 
-# Set the above Formatter to the StreamHandler
-ch.setFormatter(formatter)
+# Set the above Formatter to the StreamHandler so that all logs output to the console will follow the given format
+sh.setFormatter(formatter)
 
-# Add the StreamHandler to the Logger
-logger.addHandler(ch)
+# Add the StreamHandler to the Logger for the logging to take place
+logger.addHandler(sh)
 
 
 def index(request):
     # Assign each request a new UUID to uniquely identify it
     request.session['id'] = str(uuid.uuid4())
 
-    # Log when a new user session is created
-    logger.warning(f'New user with ID: {request.session["id"]}')
+    # Log an information message when a new user accesses the index page
+    logger.info(f'New user with ID: {request.session["id"]}')
 
     return render(request, 'index.html', {})
