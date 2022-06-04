651c75c71fabf57184bc06e68cf3ac8821512591
woojiahao <woojiahao1234@gmail.com>
2022-06-04 19:59:31 +0800

    Remove TODOs in index.py
    index.py will be a guided tutorial of how logging independent of Django
    works so we do not need the readers to actually implement anything

--- horus/views/index.py
+++ horus/views/index.py
@@ -6,26 +6,30 @@ from django.shortcuts import render
 """
 Start your logging journey here!
 
-We will first create a basic Logger fit with Handlers and Formatters.
+We will first setup a basic logging structure using the default logging features Python provides through the logging
+module.
+
+We will do so by creating a Logger and attaching a StreamHandler and Formatter, then we will output when a new user
+session is created.
 """
 
-# TODO: Create a new logger with name 'horus.views.index'
+# Create a new logger with name 'horus.views.index'
 logger = logging.getLogger('horus.views.index')
 
-# TODO: Create a StreamHandler to output all logs to console
+# Create a StreamHandler to output all logs to console
 ch = logging.StreamHandler()
 
-# TODO: Set StreamHandler level to include all INFO messages
+# Set StreamHandler level to include all INFO messages
 ch.setLevel(logging.INFO)
 
-# TODO: Create a Formatter that will output the following format: '<name> at <timestamp> (<level>) :: <message>'
+# Create a Formatter that will output the following format: '<name> at <timestamp> (<level>) :: <message>'
 formatter = logging.Formatter(
     '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-# TODO: Set the above Formatter to the StreamHandler
+# Set the above Formatter to the StreamHandler
 ch.setFormatter(formatter)
 
-# TODO: Add the StreamHandler to the Logger
+# Add the StreamHandler to the Logger
 logger.addHandler(ch)
 
 
@@ -33,7 +37,7 @@ def index(request):
     # Assign each request a new UUID to uniquely identify it
     request.session['id'] = str(uuid.uuid4())
 
-    # TODO: Log when a new user session is created
+    # Log when a new user session is created
     logger.warning(f'New user with ID: {request.session["id"]}')
 
     return render(request, 'index.html', {})
