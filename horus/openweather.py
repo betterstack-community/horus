3ccb72d2f820ed4dc14b422d9bf2787da1766410
woojiahao <woojiahao1234@gmail.com>
2022-06-04 20:20:53 +0800

    Add TODOs for openweather for Django-indp logging

--- horus/openweather.py
+++ horus/openweather.py
@@ -2,11 +2,37 @@ import environ
 import pycountry
 import requests
 
-# TODO: Enable logging with requests library
+"""
+Practice setting up Django-independent logging here!
+
+Like in index.py, we will setup a basic logging structure, but this time, create a handler to save all warnings to a
+file while logging all messages to the console as per usual.
+
+To improve the usefulness of the log messages, follow these three simple steps:
+1. Be as specific as possible about the site of the log
+2. Use appropriate log levels that correspond to the respective severities of the events
+3. Include as much information about the log, instead of just printing results
+"""
 
 env = environ.Env()
 env.read_env(env.str('ENV_PATH', '.env'))
+
+# TODO: Demonstrate a bad example of logging by logging the API key to the console
 key = env('OPEN_WEATHER_API_KEY')
+
+# TODO: Create a new Logger with name 'horus.openweather'
+
+# TODO: Set the Logger level to include all INFO and above messages
+
+# TODO: Create a StreamHandler to output all logs to the console
+
+# TODO: Create a FileHandler to save all WARNING and above logs to a file
+
+# TODO: Create a Formatter that will output the following format: '<name> at <timestamp> (level) :: <message>'
+
+# TODO: Set the above Formatter to both the StreamHandler and FileHandler
+
+# TODO: Add both the StreamHandler and FileHandler to the Logger
 
 
 def __request__(api, params):
@@ -16,7 +42,9 @@ def __request__(api, params):
 
 
 def search_countries(search, limit=5):
-    # TODO: Log the locations found from the API request
+    # TODO: Log the search submitted
+
+    # TODO: Log the results of the API request
     response = __request__(
         'http://api.openweathermap.org/geo/1.0/direct',
         {'q': search, 'limit': limit}
@@ -26,6 +54,7 @@ def search_countries(search, limit=5):
         # TODO: Log when the returned values are invalid
         return (False, [])
 
+    # TODO: Log the parsed locations
     locations = [{
         'name': location['name'],
         'lat': location['lat'],
@@ -38,14 +67,19 @@ def search_countries(search, limit=5):
 
 
 def get_current_weather(location, lat, lon):
+    # TODO: Log the arguments of this function
+
+    # TODO: Log the results of the API request
     response = __request__(
         'https://api.openweathermap.org/data/2.5/weather',
         {'lat': lat, 'lon': lon, 'units': 'metric'}
     )
 
     if not response.ok:
+        # TODO: Log when the API request fails
         return (False, {})
 
+    # TODO: Log the parsed weather
     weather = {
         'current': response.json()['weather'][0]['description'].lower(),
         'temp': response.json()['main']['temp'],
