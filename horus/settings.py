fb51840895d42bf851c19d6f55cc8d1355eee451
Woo Jia Hao <woojiahao1234@gmail.com>
2022-07-31 12:56:33 +0800

    Remove Django and requests logs

--- horus/settings.py
+++ horus/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from ensurepip import version
 from pathlib import Path
+import logging.config
 
 import environ
 
@@ -23,7 +24,6 @@ env.read_env(env.str('ENV_PATH', '.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -36,7 +36,6 @@ DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = [
 ]
-
 
 # Application definition
 
@@ -79,7 +78,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'horus.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -89,7 +87,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -109,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -120,7 +116,6 @@ TIME_ZONE = 'Asia/Singapore'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
@@ -138,8 +133,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 # Logging configurations
+# Disable Django's built-in logging system
+LOGGING_CONFIG = None
+
 # Once you have understood how the logging configurations and search.py is implemented, work on the TODOs
 LOGGING = {
     'version': 1,
@@ -177,3 +174,9 @@ LOGGING = {
         # TODO: Define a new logger called 'horus.views.weather', it will use both the 'console' and 'file' handler
     }
 }
+
+# Disable requests library built-in logging
+logging.getLogger("requests").setLevel(logging.ERROR)
+
+# Implement our own logging system
+logging.config.dictConfig(LOGGING)
